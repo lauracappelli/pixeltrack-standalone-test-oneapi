6114bfcceb86697433a5eb94b3786fac141ced61
lauracappelli <laura.cappelli6@studio.unibo.it>
2020-09-01 15:06:22 +0200

    pulizia cartella

--- src/kokkostest/KokkosCore/kokkoshost/ProductBase.cc
+++
@@ -1,9 +0,0 @@
-#include "KokkosCore/ProductBase.h"
-
-namespace cms {
-  namespace kokkos {
-    namespace impl {
-      ExecSpaceSpecificBase::~ExecSpaceSpecificBase() = default;
-    }
-  }  // namespace kokkos
-}  // namespace cms
