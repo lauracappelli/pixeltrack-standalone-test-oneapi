8f0c639b103d5baafda192f7801b30c71cea9910
Viola <viola.cavallini@student.unife.it>
2020-09-04 16:54:04 +0200

    spostati due file in 'cute'

---
+++ pixel-standalone/cute/PixelErrors.h
@@ -0,0 +1,23 @@
+#ifndef DataFormats_SiPixelDigi_interface_PixelErrors_h
+#define DataFormats_SiPixelDigi_interface_PixelErrors_h
+
+#include <CL/sycl.hpp>
+#include <dpct/dpct.hpp>
+#include <map>
+#include <vector>
+#include <cstdint>
+
+#include "SiPixelRawDataError.h"
+
+// Better ideas for the placement of these?
+
+struct PixelErrorCompact {
+  uint32_t rawId;
+  uint32_t word;
+  uint8_t errorType;
+  uint8_t fedId;
+};
+
+using PixelFormatterErrors = std::map<uint32_t, std::vector<SiPixelRawDataError>>;
+
+#endif  // DataFormats_SiPixelDigi_interface_PixelErrors_h
